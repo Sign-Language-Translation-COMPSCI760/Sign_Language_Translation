09c11cbe6c9f79440e95077afaa41f88d6e5a09f
RSpe <me@rspe.xyz>
2020-08-16 21:00:18 +1200

    Move test.py into tidy_data

--- dataset/tidy_data.py
+++ dataset/tidy_data.py
@@ -1,1 +1,27 @@
-print("hello world!")
+import pandas as pd
+import math
+
+def main():
+    
+    file = pd.read_excel("dai-asllvd-BU_glossing_with_variations_HS_information-extended-urls-RU.xlsx", sheet_name="Sheet1")
+    words = {}      #key = words, value = number of occurences
+    firstline = True
+    for i in file.index:
+        
+        if firstline:    #skip first line
+            firstline = False
+            continue
+    
+        if file['Main New Gloss'][i] == " " or str(file['Main New Gloss'][i]) == "nan":
+            words[current] += 1 
+        else:
+            current = file['Main New Gloss'][i]
+            words[current] = 0
+
+    with open("test examples.txt", mode='w') as fout:
+        for key, value in words.items():
+            if value >= 10:
+                fout.write(key+"\n")
+            
+        
+main()
