a90ea3ed426b1a7df9805e8d9fd47f5228f03b31
RSpe <me@rspe.xyz>
2020-08-17 17:09:29 +1200

    Downloads signed video files

--- dataset/tidy_and_download_data.py
+++ dataset/tidy_and_download_data.py
@@ -1,3 +1,4 @@
+from openpyxl import load_workbook
 import urllib.request
 import pandas as pd
 import math
@@ -15,9 +16,8 @@ def find_class_occurance_and_download_video(excel_file):
     
         if excel_file['Main New Gloss'][i] == " " or str(excel_file['Main New Gloss'][i]) == "nan":
             if excel_file['Consultant'][i] != "------------":
-                #video_link = excel_file['Separate'][i]
-                #urllib.request.urlretrieve(video_link, current + "__" + current) 
-                print(excel_file['Separate'][i])
+                video_link = excel_file['Links seperate 1'][i][12:-9]
+                urllib.request.urlretrieve(video_link, current + "__" + str(words[current] + 1) + ".mov") 
                 words[current] += 1 
         elif excel_file['Consultant'][i] != "------------":
             current = excel_file['Main New Gloss'][i]
@@ -30,6 +30,7 @@ def find_class_occurance_and_download_video(excel_file):
 def main():
     
     excel_file = pd.read_excel("dai-asllvd-BU_glossing_with_variations_HS_information-extended-urls-RU.xlsx", sheet_name="Sheet1")
+    #print(excel_file)
 
     find_class_occurance_and_download_video(excel_file) # Outputs a csv containing all the different signs and how many times they are signed in videos (in the dataset). Downloads and sanitisies video names and stores them in a video folder.
         
