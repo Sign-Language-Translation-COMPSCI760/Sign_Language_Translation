f796b0e68daccec07fc2299b1f4c4dbbb81a294c
sun <sjun521@aucklanduni.ac.nz>
2020-10-02 21:49:44 +1300

    changed code so it renames the file better

--- combine_video.py
+++ combine_video.py
@@ -8,6 +8,7 @@ def combine():
     for file in os.listdir(os.getcwd()):
         if file.endswith(".png"):  # image file type (need to change)
             framesList1.append(cv2.imread(file))
+            name = file
 
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
     sz = (480, 368) #video dimensions
@@ -15,7 +16,7 @@ def combine():
 
 
     out1 = cv2.VideoWriter() # if a different file type is wanting to be used change code here
-    out1.open(file[:-4]+".mp4" ,fourcc,fps,sz,True)
+    out1.open(name[:-4]+".mp4" ,fourcc,fps,sz,True)
 
     for frame in framesList1: #writing video frames in list to video
         out1.write(frame)
