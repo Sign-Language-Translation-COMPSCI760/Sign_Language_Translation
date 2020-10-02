b4304d87be6d21b5b0b43c9fa73b884d08b2d91f
sun <sjun521@aucklanduni.ac.nz>
2020-10-02 21:45:18 +1300

    combines all images in the current folder (need to change by file extension e.g. ".png")

---
+++ combine_video.py
@@ -0,0 +1,29 @@
+import cv2
+import os
+def combine():
+
+    framesList1 = []
+    fps = 15
+
+    for file in os.listdir(os.getcwd()):
+        if file.endswith(".png"):  # image file type (need to change)
+            framesList1.append(cv2.imread(file))
+
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    sz = (480, 368) #video dimensions
+
+
+
+    out1 = cv2.VideoWriter() # if a different file type is wanting to be used change code here
+    out1.open(file[:-4]+".mp4" ,fourcc,fps,sz,True)
+
+    for frame in framesList1: #writing video frames in list to video
+        out1.write(frame)
+
+    out1.release()
+
+
+combine()
+
+
+
